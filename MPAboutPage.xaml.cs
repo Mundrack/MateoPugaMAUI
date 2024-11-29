755774cd9afac0dbfdf4903ea9d7e52c5619105d
Mundrack <mateopuga75@gmail.com>
2024-11-29 07:45:02 -0500

    Creamos es espacio MAUI y escojimos el emulador de android. Tambien creamos un nuevo XAMAL llamado AbautPage Tambien agregams un codigo en el XAML de AboutPage y agregamos en el XAML.cs una funsion

---
+++ MPAboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace MateoPugaMAUI;
+
+public partial class MPAboutPage : ContentPage
+{
+	public MPAboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
